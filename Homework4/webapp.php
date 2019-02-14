7ce6555c5562e5f3f16a6a96c6b73870bf795804
Cameron Clark <cpc1007@rit.edu>
2019-02-14 18:14:59 -0500

    add php app for assignment

---
+++ Homework4/webapp.php
@@ -0,0 +1,60 @@
+<?php
+    // Small web app to write/read/delete files in /tmp/webapp
+    $directory = "/tmp/webapp/";
+    if (!is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+
+    // Get parameters
+    $method = $_GET['method'];
+    $fname = $_GET['name'];
+    $contents = $_GET['contents'];
+
+    // Santize
+    $fname = escapeshellcmd($fname);
+    $fname = basename($fname);
+    $filepath = $directory . $fname;
+    echo nl2br($fname . "\n");
+    echo nl2br($filepath . "\n");
+
+    if ($filepath === false || strpos($filepath, $directory) !== 0) {
+        http_response_code(400);
+        echo "Can only write/read files in /tmp/webapp";
+    } else {
+        execute_method($method, $filepath, $contents);
+    }
+
+    
+    // Determine method to execute
+    function execute_method($method, $filepath, $contents) {
+        if (strcmp($method, 'create') == 0) {
+            create_file($filepath, $contents);
+        } elseif (strcmp($method, 'del') == 0) {
+            delete_file($filepath);
+        } elseif (strcmp($method, 'view') == 0) {
+            view_file($filepath);
+        } else {
+            // return error
+            http_response_code(400);
+            echo "Method not supported";
+        }
+    }
+
+
+    function create_file($name, $contents) {
+        $file = fopen($name, "w") or die("Unable to open file!");
+        fwrite($file, $contents);
+        echo "File Created";
+        fclose($file);
+    }
+
+    function delete_file($name) {
+        unlink($name);
+        echo "File Deleted";
+    }
+
+    function view_file($name) {
+        $contents = file_get_contents($name) or die("Unable to open file!");
+        echo $contents;
+    }
+?>
