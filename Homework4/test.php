4b9d8f6281ef44532349ebd0635591531898cb41
Cameron Clark <cpc1007@rit.edu>
2019-02-23 15:38:33 -0500

    Adding homework5

--- Homework4/test.php
+++ Homework4/test.php
@@ -1,4 +1,5 @@
 <?php
+// test functions for webapp
 use PHPUnit\Framework\TestCase;
 include("webapp.php");
 
@@ -22,8 +23,24 @@ class WebAppTest extends TestCase
         $name = "test.txt; nc -nlvp 8000";
         $contents = "foobar";
         $this->assertTrue(main("create", $name, $contents));
-
-
+        $connected = false;
+        try 
+        {
+            $sock = fsockopen("127.0.0.1", 8000, $errnum, $errstr, 45);
+            if (!$sock)
+            {
+                $connected = false;
+            }
+            else 
+            {
+                $connected = true;
+            }
+        }
+        catch (Exception $e)
+        {
+            $connected = false;
+        }
+        $this->assertFalse($connected);
         $this->assertTrue(main("del", $name, $contents));
     }
 
